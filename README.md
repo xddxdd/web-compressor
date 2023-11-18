7c8e79dea7af1afad562b43268129ebbc72b0bb3
Lan Tian <xuyh0120@outlook.com>
2023-11-18 11:19:05 -0800

    Add parameter for include/exclude extensions

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A simple Rust program that pre-compresses your static web assets.
 ## Usage
 
 ```bash
-./web-compressor path/to/compress
+./web-compressor --target path/to/compress
 ```
 
 This program will compress **everything** in the target folder into:
@@ -15,6 +15,22 @@ This program will compress **everything** in the target folder into:
 - Zstd `.zst`: only if smaller than original file, Gzip and Brotli
 
 This program will run compression steps in parallel, utilizing all CPU cores.
+
+All options:
+
+```bash
+Usage: web-compressor [OPTIONS] --target <TARGET>
+
+Options:
+  -t, --target <TARGET>            Folder to compress
+  -i, --include-ext <INCLUDE_EXT>  Extensions to be compressed [default: html,css,js,atom,stl,xml,svg,json,txt]
+  -e, --exclude-ext <EXCLUDE_EXT>  Extensions to be not compressed [default: ]
+  -g, --gzip                       Enable Gzip compression
+  -b, --brotli                     Enable Brotli compression
+  -z, --zstd                       Enable Zstd compression
+  -h, --help                       Print help
+  -V, --version                    Print version
+```
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,81 @@
 mod compress;
 
-use std::env;
+use clap::Parser;
 use std::fs;
 use walkdir::WalkDir;
 
-fn main() {
-    if env::args().count() != 2 {
-        let argv_0 = env::args().nth(0).unwrap();
-        println!("Usage: {} path/to/compress", argv_0);
-        std::process::exit(1);
-    }
+const COMPRESSED_EXTS: &[&str] = &["gz", "br", "zstd"];
 
-    let dir = fs::canonicalize(env::args().nth(1).unwrap())
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Folder to compress
+    #[arg(short, long)]
+    target: String,
+
+    /// Extensions to be compressed
+    #[arg(short, long, default_value = "html,css,js,atom,stl,xml,svg,json,txt")]
+    include_ext: String,
+
+    /// Extensions to be not compressed
+    #[arg(short, long, default_value = "")]
+    exclude_ext: String,
+
+    /// Enable Gzip compression
+    #[arg(short, long, default_value_t = true)]
+    gzip: bool,
+
+    /// Enable Brotli compression
+    #[arg(short, long, default_value_t = true)]
+    brotli: bool,
+
+    /// Enable Zstd compression
+    #[arg(short, long, default_value_t = true)]
+    zstd: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let dir = fs::canonicalize(args.target.clone())
         .unwrap()
         .to_str()
         .unwrap()
         .to_owned();
 
-    iterate_directory(dir);
+    iterate_directory(dir, &args);
 }
 
-fn is_compressed(file: String) -> bool {
-    return file.ends_with(".gz") || file.ends_with(".br") || file.ends_with(".zstd");
+fn is_compressed(file: &String) -> bool {
+    return has_extension(file, &COMPRESSED_EXTS.iter().map(|s| *s).collect());
 }
 
-fn iterate_directory(dir: String) {
+fn has_extension(file: &String, exts: &Vec<&str>) -> bool {
+    return exts.iter().any(|e| {
+        file.to_lowercase()
+            .ends_with(&format!(".{}", e.to_lowercase()))
+    });
+}
+
+fn iterate_directory(dir: String, args: &Args) {
+    let include_exts: Vec<&str> = args
+        .include_ext
+        .split(",")
+        .filter(|s| !s.is_empty())
+        .collect();
+    let exclude_exts: Vec<&str> = args
+        .exclude_ext
+        .split(",")
+        .filter(|s| !s.is_empty())
+        .collect();
+    println!("Included extensions: {}", include_exts.join(", "));
+    println!("Excluded extensions: {}", exclude_exts.join(", "));
+
     rayon::scope(|scope| {
         println!("Scanning {:?}", dir);
         let walker = WalkDir::new(dir)
             .into_iter()
-            .filter_entry(|e| !is_compressed(e.path().to_str().unwrap().to_owned()));
+            .filter_entry(|e| !is_compressed(&e.path().to_str().unwrap().to_owned()));
 
         for it in walker {
             let entry = it.unwrap();
@@ -39,8 +85,18 @@ fn iterate_directory(dir: String) {
                 continue;
             }
 
+            if !include_exts.is_empty() && !has_extension(&path, &include_exts) {
+                println!("Skipped {} for not the file type to include", path.clone());
+                continue;
+            }
+
+            if !exclude_exts.is_empty() && has_extension(&path, &exclude_exts) {
+                println!("Skipped {} for being file type to exclude", path.clone());
+                continue;
+            }
+
             scope.spawn(move |_| {
-                let max_size = match compress::file_size(path.clone()) {
+                let mut max_size = match compress::file_size(path.clone()) {
                     Ok(len) => len,
                     Err(e) => {
                         println!("Error processing {}: {}", path.clone(), e.to_string());
@@ -49,12 +105,18 @@ fn iterate_directory(dir: String) {
                 };
 
                 // Sorted by browser compatibility, prefer older format if smaller
-                let size = compress::gzip(path.clone(), max_size).unwrap();
-                let max_size = std::cmp::min(max_size, size);
-                let size = compress::brotli(path.clone(), max_size).unwrap();
-                let max_size = std::cmp::min(max_size, size);
-                let size = compress::zstd(path.clone(), max_size).unwrap();
-                let max_size = std::cmp::min(max_size, size);
+                if args.gzip {
+                    let size = compress::gzip(path.clone(), max_size).unwrap();
+                    max_size = std::cmp::min(max_size, size);
+                }
+                if args.brotli {
+                    let size = compress::brotli(path.clone(), max_size).unwrap();
+                    max_size = std::cmp::min(max_size, size);
+                }
+                if args.zstd {
+                    let size = compress::zstd(path.clone(), max_size).unwrap();
+                    max_size = std::cmp::min(max_size, size);
+                }
 
                 // Suppress unused code warning
                 let _ = max_size;
